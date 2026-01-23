34f28d925c711595235f1f8e05b2eca07cc28660
Vinitgaikwad <vinitgaikwad041@gmail.com>
2026-01-23 20:37:12 +0530

    Added Navbar for landing page

--- frontend/src/shared/components/landingNavbar/LandingNavbar.tsx
+++ frontend/src/shared/components/landingNavbar/LandingNavbar.tsx
@@ -1,7 +1,23 @@
+import { NavLink } from "react-router-dom";
+import Logo from "../../../assets/logo.png"
+
 export function LandingNavbar() {
     return (
-        <nav>
-            Landing Page Navbar
+        <nav className="w-[100%] flex justify-center ">
+            <div className="flex px-2 justify-between items-center shadow-md shadow-gray-300 w-full px-24">
+
+                <div className="flex-1">
+                    <img src={Logo} alt="logo" className="w-16" />
+                </div>
+                <div className="flex-auto hidden">
+
+                </div>
+                <div className="flex-1 flex pr-4 md:pr-0">
+                    <button className="border border-black ml-auto">
+                        <NavLink to="/login">Sign In</NavLink>
+                    </button>
+                </div>
+            </div>
         </nav>
     );
 }
